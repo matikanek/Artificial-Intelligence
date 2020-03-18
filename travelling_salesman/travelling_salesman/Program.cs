30ea59f8ed39a81b971233e584ab568274d8d040
matikanek <291564.umk@gmail.com>
2020-03-18 13:15:30 +0100

    Input data for simulated annealing

--- travelling_salesman/travelling_salesman/Program.cs
+++ travelling_salesman/travelling_salesman/Program.cs
@@ -8,16 +8,33 @@ namespace travelling_salesman
 {
     class Program
     {
-        public static string[] alfabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "W", "X", "Y", "Z" };
+        public static char[] alfabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z' };
         public static double[] X = { 1, 2, 4, 8 };
         public static double[] Y = { 4, 1, 3, 5 };
 
+        public class Track
+        {
+            public string combination;
+            public double value;
+            public int index;
+            public Track(string _c, double _v ,int _i)
+            {
+                combination = _c;
+                value = _v;
+                index = _i;
+            }
+            public override string ToString()
+            {
+                return "Track " + combination + ", distance: " + value + ", index: " + index;
+            }
+        }
+
         public class Point
         {
-            public string name;
+            public char name;
             public double x;
             public double y;
-            public Point(string _n, double _x, double _y)
+            public Point(char _n, double _x, double _y)
             {
                 name = _n;
                 x = _x;
@@ -29,7 +46,61 @@ namespace travelling_salesman
             }
         }
 
-        private static List<string> Permute(string str, int l, int r, List<string> list)
+        public static double Pythagoras(Point a, Point b)
+        {
+            return Math.Sqrt(Math.Pow(b.x - a.x, 2) + Math.Pow(b.y - a.y, 2));
+        }
+
+        public static Point Search(char c, List<Point> points)
+        {
+            int i = 0;
+            while (c != points[i].name) i++;
+            return points[i];
+        }
+
+        public static double CountLengthOfTrack(string combination, List<Point> points)
+        {
+            double result = 0;
+            char prev = combination[0];
+            char next = combination[1];
+            Point dataPrev = Search(prev, points);
+            Point dataNext = Search(next, points);
+            for (int i=2; i < combination.Length; i++)
+            {
+                result += Pythagoras(dataPrev, dataNext);
+                prev = next;
+                next = combination[i];
+                dataPrev = Search(prev, points);
+                dataNext = Search(next, points);
+            }
+            result += Pythagoras(dataPrev, dataNext);
+            return result;
+        }
+
+        public static List<Track> AssignLengthOfTracks(List<string> ListOfCombinations, List<Point> ListOfPoints)
+        {
+            List<Track> list = new List<Track>();
+            for(int i=0; i < ListOfCombinations.Count; i++)
+            {
+                double tmp = CountLengthOfTrack(ListOfCombinations[i], ListOfPoints);
+                list.Add(new Track(ListOfCombinations[i], tmp, i));
+            }
+            return list;
+        }
+
+        public static List<string> Permute(List<Point> ListOfPoints, int l, int r)
+        {
+            List<string> list = new List<string>();
+            string str = "";
+            for (int i = 0; i < ListOfPoints.Count; i++)
+            {
+                str += ListOfPoints[i].name;
+            }
+            list = ToPermute(str, l, r, list);
+            return list;
+        }
+
+        private static List<string> ToPermute(string str, int l, int r, List<string> list)
         {
             if (l == r)
                 list.Add(str);
@@ -38,7 +109,7 @@ namespace travelling_salesman
                 for (int i = l; i <= r; i++)
                 {
                     str = Swap(str, l, i);
-                    Permute(str, l + 1, r, list);
+                    ToPermute(str, l + 1, r, list);
                     str = Swap(str, l, i);
                 }
             }
@@ -60,7 +131,7 @@ namespace travelling_salesman
             List<Point> ListOfPoints = new List<Point>();
             for (int i = 0; i < amount; i++)
             {
-                string alf = alfabet[i];
+                char alf = alfabet[i];
                 Point tmp = new Point(alf, X[i], Y[i]);
                 ListOfPoints.Add(tmp);
             }
@@ -76,19 +147,16 @@ namespace travelling_salesman
         static void Main(string[] args)
         {
             int N = 4;
-            string str = "";
             List<Point> ListOfPoints = new List<Point>();
-            List<string> ListOfConbinationS = new List<string>();
+            List<string> ListOfConbinations = new List<string>();
+            List<Track> ListOfTracks = new List<Track>();
             ListOfPoints = CreatePoints(N);
-            for(int i=0; i<ListOfPoints.Count; i++)
-            {
-                str += ListOfPoints[i].name;
-            }
-            ListOfConbinationS = Permute(str, 0, N - 1, ListOfConbinationS);
+            ListOfConbinations = Permute(ListOfPoints, 0, N - 1);
+            ListOfTracks = AssignLengthOfTracks(ListOfConbinations, ListOfPoints);
 
-            for (int k = 0; k < ListOfConbinationS.Count; k++)
+            for(int i=0; i < ListOfTracks.Count; i++)
             {
-                Console.WriteLine(ListOfConbinationS[k]);
+                Console.WriteLine(ListOfTracks[i]);
             }
 
             Console.ReadKey();
